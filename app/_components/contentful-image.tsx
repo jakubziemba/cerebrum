415397fa63060f1dc3aa6a0df3d860e25b08191a
jakubziemba <ziemba.jak@gmail.com>
2023-12-06 18:47:58 +0100

    Init

---
+++ app/_components/contentful-image.tsx
@@ -0,0 +1,19 @@
+// Might be irrelevant but this component was originally in /lib folder in the root of the project
+'use client';
+
+import Image from 'next/image';
+
+interface ContentfulImageProps {
+  src: string;
+  width?: number;
+  quality?: number;
+  [key: string]: any; // For other props that might be passed
+}
+
+const contentfulLoader = ({ src, width, quality }: ContentfulImageProps) => {
+  return `${src}?w=${width}&q=${quality || 75}`;
+};
+
+export default function ContentfulImage(props: ContentfulImageProps) {
+  return <Image alt={props.alt} loader={contentfulLoader} {...props} />;
+}
